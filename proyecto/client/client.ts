8debc35267c3e41f2c7294b4112f28ede1dc7059
maxteam547272 <L202407030@vbravo.tecnm.mx>
2026-04-27 02:43:43 -0600

    Update client.ts

--- proyecto/client/client.ts
+++ proyecto/client/client.ts
@@ -1,7 +1,40 @@
-// Client
-console.log("My address:", pg.wallet.publicKey.toString());
-console.log("Program ID:", pg.PROGRAM_ID.toString());
-const balance = await pg.connection.getBalance(pg.wallet.publicKey);
-console.log(`My balance: ${balance / web3.LAMPORTS_PER_SOL} SOL`);
-const txHash = await pg.program.methods.mi_primer_instruccion().rpc();
-console.log(txHash);
+import * as anchor from "@coral-xyz/anchor";
+
+describe("Sistema de RPG Solana", () => {
+  const provider = anchor.AnchorProvider.env();
+  anchor.setProvider(provider);
+  const program = pg.program;
+  const jugadorKeypair = anchor.web3.Keypair.generate();
+
+  it("¡Crea un nuevo Guerrero!", async () => {
+    await program.methods
+      .iniciarJugador("Ruth_Master")
+      .accounts({
+        jugador: jugadorKeypair.publicKey,
+        user: provider.wallet.publicKey,
+        systemProgram: anchor.web3.SystemProgram.programId,
+      })
+      .signers([jugadorKeypair])
+      .rpc();
+
+    const cuenta = await program.account.jugador.fetch(
+      jugadorKeypair.publicKey
+    );
+    console.log("Nombre del Heroe:", cuenta.nombre);
+    console.log("Nivel Inicial:", cuenta.nivel.toString());
+  });
+
+  it("¡Gana batallas y sube de nivel!", async () => {
+    await program.methods
+      .ganarExperiencia()
+      .accounts({
+        jugador: jugadorKeypair.publicKey,
+      })
+      .rpc();
+
+    const cuenta = await program.account.jugador.fetch(
+      jugadorKeypair.publicKey
+    );
+    console.log("XP actual:", cuenta.xp.toString());
+  });
+});
